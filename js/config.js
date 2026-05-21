61eed614c3ae8f61c1cf32b4c6fbe1758fa97028
mulloc1 <skybgk1@gmail.com>
2026-05-21 19:31:42 +0900

    chore: scaffold portfolio html, css tokens, js modules

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const GITHUB_USERNAME = "";
+export const SCROLL_TOP_THRESHOLD = 300;
+export const NAV_BG_THRESHOLD = 60;
+export const OBSERVER_THRESHOLD = 0.2;
---
+++ js/main.js
@@ -0,0 +1,11 @@
+import { init as initTheme } from "./theme.js";
+import { init as initNav } from "./nav.js";
+import { init as initAnimate } from "./animate.js";
+import { init as initProjects } from "./projects.js";
+import { init as initContact } from "./contact.js";
+
+initTheme();
+initNav();
+initAnimate();
+initProjects();
+initContact();
